74d34841827cb7c1d3893f7c67eefca6a0288c34
Matthew Dumont <mjdumont1@gmail.com>
2011-10-20 00:51:05 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conference2012::Application.config.secret_token = '4eb72374744398c9a4d4a9f63d176e4426dd2dc279a1ac8d4f587b4a944967d6d18969f256db1adb5d4be5a1fe012e02dec30308fe98216053a63efd39e0eb62'
