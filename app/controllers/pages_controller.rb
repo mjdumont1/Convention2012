8072fdc3957eb6b493eea9625fe05fa2de6e12b6
Matthew Dumont <mjdumont1@gmail.com>
2011-10-30 20:33:58 -0600

    Updating Titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,82 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
   end
 
-  def agenda
+  def program
+	@title = "Program"
   end
 
-  def feature_speakers_and_events
+  def schedule
+	@title = "Schedule"
   end
 
   def sponsors
+	@title = "Sponsors"
   end
 
   def contact_us
+	@title = "Contact Us"
   end
 
-  def photo_gallery
+  def speakers
+	@title = "Speakers"
+  end
+
+  def registration
+	@title = "Registration"
+  end
+
+  def educationals
+	@title = "Educationals"
+  end
+
+  def speaker_bios
+	@title = "Speaker Bios"
+  end
+
+  def faq
+	@title = "FAQ"
+  end
+
+  def prizes
+	@title = "Prizes"
+  end
+
+  def terms_conditions
+	@title = "Terms & Conditions"
+  end
+
+  def venue
+	@title = "Venue"
+  end
+
+  def about_calgary
+	@title = "About Calgary"
+  end
+
+  def our_sponsors
+	@title = "Our Sponsors"
+  end
+
+  def volunteers
+	@title = "Volunteers"
+  end
+
+  def application
+	@title = "Application"
+  end
+
+  def about
+	@title = "About"
+  end
+
+  def planning_team
+	@title = "Planning Team"
+  end
+
+  def disclaimer
+	@title = "Disclaimer"
   end
 
 end
